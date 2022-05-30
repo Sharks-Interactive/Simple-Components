8fce074f1a41dc91264aaa5994b2e3879bdb62dc
Owen Silva <owensilva1282@gmail.com>
2022-05-30 12:37:32 -0400

    🎥v1.2.1 Eslint conformities to fix build

--- src/simple-components.js
+++ src/simple-components.js
@@ -1,10 +1,9 @@
+/* eslint-disable max-len */
 /**
  * Creates a new simpleComps instance
  * @param {string} folder - Path to the folder containing comp files
  */
 export function SimpleComps(folder) {
-  const _this = this;
-
   /**
    * Renders the specified comp
    * @param {string} component - The name of the comp to render
@@ -15,42 +14,51 @@ export function SimpleComps(folder) {
     const promise = new Promise();
 
     fetch(folder + component + '.html')
-      .then((res) => {
-        return res.text();
-      })
-      .then((componentFile) => {
-        const componentData = componentFile;
+        .then((res) => {
+          return res.text();
+        })
+        .then((componentFile) => {
+          const componentData = componentFile;
 
-        for (let i = 0; i < inputs.length; i++) {
-          if (componentData == undefined) { promise.resolve(); return promise; } // Error
+          for (let i = 0; i < inputs.length; i++) {
+            if (componentData == undefined) {
+              promise.resolve();
+              return promise;
+            } // Error
 
-          let data = componentData;
+            let data = componentData;
 
-          const newElement = document.createElement('div');
-          const newScript = document.createElement('script'); // Add script tag (If we need it)
+            const newElement = document.createElement('div');
+            const newScript = document.createElement('script'); // Add script tag (If we need it)
 
-          data = parseData(data, inputs.item(i));
+            data = parseData(data, inputs.item(i));
 
-          if (data.includes('script')) {
-            const script = data.match(/\<script(.*?)>(.+)\<\/script>/gs)[0];
-            newScript.innerHTML = script.replace(/(<script(.*?)>|<\/script>)/gs, ''); // Remove script tags
+            if (data.includes('script')) {
+              const script = data.match(/\<script(.*?)>(.+)\<\/script>/gs)[0];
+              newScript.innerHTML = script.replace(
+                  /(<script(.*?)>|<\/script>)/gs,
+                  '',
+              ); // Remove script tags
 
-            data = data.replace(/\<script(.*?)>(.+)\<\/script>/gs, ''); // Remove code from innerHTML
-            try { newScript.type = script.match(/type="(.*?)"/gs)[0].replace(/type="|"/gs, ""); } catch { } // If it has one, set the scripts type
+              data = data.replace(/\<script(.*?)>(.+)\<\/script>/gs, ''); // Remove code from innerHTML
+              try {
+                newScript.type = script
+                    .match(/type="(.*?)"/gs)[0]
+                    .replace(/type="|"/gs, '');
+              } catch {} // If it has one, set the scripts type
+            }
+
+            newElement.innerHTML = data;
+
+            parseIfs(newElement);
+
+            inputs.item(i).appendChild(newElement);
+            /* IMPORTANT: If your seeing errors on this line it means that you have an error in your components code! */
+            if (newScript != null) newElement.appendChild(newScript); // Add script to element
+
+            if (i == inputs.length - 1) promise.resolve();
           }
-
-          newElement.innerHTML = data;
-
-          parseIfs(newElement);
-
-          inputs.item(i).appendChild(newElement);
-          /* IMPORTANT: If your seeing errors on this line it means that you have an error in your components code! */
-          if (newScript != null)
-            newElement.appendChild(newScript); // Add script to element
-
-          if (i == inputs.length - 1) promise.resolve();
-        }
-      });
+        });
 
     return promise;
   };
@@ -60,9 +68,14 @@ export function SimpleComps(folder) {
    * @param {string} component - The type of the comps to create
    * @param {int} number - The number of this comp to render
    * @param {HTMLElement} parent - The parent to create the comps under
-   * @param {object} data - JSON stringified object with a list of lists with equivalent attribute arrays named attrs for the duplicates in the format of ATTR_NAME | ATTR_VALUE
+   * @param {object} attributes - JSON stringified object with a list of lists with equivalent attribute arrays named attrs for the duplicates in the format of ATTR_NAME | ATTR_VALUE
    */
-  this.create = (component, number = 1, parent = document.body, attributes = null) => {
+  this.create = (
+      component,
+      number = 1,
+      parent = document.body,
+      attributes = null,
+  ) => {
     for (let i = 0; i < number; i++) {
       const createdElement = document.createElement(component);
       parent.appendChild(createdElement);
@@ -70,8 +83,13 @@ export function SimpleComps(folder) {
       if (attributes == null) continue;
 
       try {
-        for (let x = 0; x < attributes.l[i].attrs.length; x++) createdElement.setAttribute(attributes.l[i].attrs[x].split('|')[0], attributes.l[i].attrs[x].split('|')[1]);
-      } catch (e) { }
+        for (let x = 0; x < attributes.l[i].attrs.length; x++) {
+          createdElement.setAttribute(
+              attributes.l[i].attrs[x].split('|')[0],
+              attributes.l[i].attrs[x].split('|')[1],
+          );
+        }
+      } catch (e) {}
     }
   };
 
@@ -90,14 +108,17 @@ export function SimpleComps(folder) {
     for (let i = 0; i < customAtts.length; i++) {
       // Remove brackets from custom data/attributes
       const _att = customAtts[i].replace(/[\{\}]+/g, '');
-      if (elem.getAttribute(_att) == undefined) { console.warn(`SKIPPING ${_att}`); continue; } // No attr of this type exists on this comp - skip it
+      if (elem.getAttribute(_att) == undefined) {
+        console.warn(`SKIPPING ${_att}`);
+        continue;
+      } // No attr of this type exists on this comp - skip it
 
       // Get value of attribute of same name on component element
       data = data.replace(customAtts[i], elem.getAttribute(_att));
     }
 
     return data;
-  }
+  };
 
   /**
    * Parses if attributes in a component
@@ -119,7 +140,7 @@ export function SimpleComps(folder) {
         if (elm.children.item(g).children.length > 0) {
           parseIfs(elm.children.item(g));
         }
-      } catch (error) { }
+      } catch (error) {}
     }
   };
 
@@ -132,15 +153,15 @@ export function SimpleComps(folder) {
   }
 
   Promise.prototype = {
-    then: function (callback) {
+    then: function(callback) {
       if (this.resolved) callback();
       else this.callbacks.push(callback); // Queue up callback for later
     },
 
-    resolve: function () {
+    resolve: function() {
       this.resolved = true;
       // Fire all callbacks
-      this.callbacks.forEach(function (callback) {
+      this.callbacks.forEach(function(callback) {
         callback();
       });
     },
